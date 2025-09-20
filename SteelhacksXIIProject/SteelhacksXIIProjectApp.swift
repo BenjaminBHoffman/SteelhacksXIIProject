24c139bce620ff55ee23fd4ffba66dbcb2389b46
BenBHoffman <beniphone@verizon.net>
2025-09-20 14:49:50 -0400

    Initial Commit

---
+++ SteelhacksXIIProject/SteelhacksXIIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteelhacksXIIProjectApp.swift
+//  SteelhacksXIIProject
+//
+//  Created by Ben Hoffman on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SteelhacksXIIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
